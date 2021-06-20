34350da22551ea57a54643f506a07ce48682ecdd
pushkarasharma <pushkarasharma11@gmail.com>
2021-06-20 16:50:12 +0530

    addd databas models

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Location, Meetup
 
-# Register your models here.
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title','slug')
+    list_filter = ('title',)
+    prepopulated_fields = {'slug':('title',)}
+
+
+admin.site.register(Meetup,MeetupAdmin)
+#admin.site.register(Location)
--- meetups/views.py
+++ meetups/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Meetup
 
 # Create your views here.
 def index(request):
-    meetups = [
-        {'title':'1st Meetup','locations':'New York','slug':'a-first-meetup'},
-        {'title':'2nd Meetup','locations':'Delhi','slug':'a-second-meetup'}
-    ]
+    meetups = Meetup.objects.all()
     return render(request, 'meetups/index.html',{
         'meetups':meetups,
-        'show_meetups':True
         })
 
 def meetup_details(request, meetup_slug):
-    print(meetup_slug)
-    selected_meetup = {
-        'title':'A first Meetup',
-        'description':'This meeting is basically for python developers'
-    }
-    return render(request, 'meetups/meetup_details.html',{
-        'meetup_title':selected_meetup['title'],
-        'description':selected_meetup['description']
-    })        
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+
+        return render(request, 'meetups/meetup_details.html',{
+            'meetup_found':True,
+            'meetup_title':selected_meetup.title,
+            'description':selected_meetup.description
+        })
+
+    except Exception as e:
+        return render(request, 'meetups/meetup_details.html',{
+            'meetup_found':False
+        })
+
